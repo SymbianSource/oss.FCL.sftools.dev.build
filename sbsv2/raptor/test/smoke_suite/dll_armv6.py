459cfc3b00fbe48e861fbb38abe8a95386ce806d
raptorbot <raptorbot@systemstesthead.symbian.intra>
2009-11-24 16:59:34 +0000

    Only a few tests left to get going
    --HG--
    branch : wip

--- sbsv2/raptor/test/smoke_suite/dll_armv6.py
+++ sbsv2/raptor/test/smoke_suite/dll_armv6.py
@@ -18,10 +18,9 @@ from raptor_tests import AntiTargetSmokeTest
 
 def run():
 	t = AntiTargetSmokeTest()
-	t.id = "97"
-	t.name = "dll_armv6"
-	t.command = "sbs -b smoke_suite/test_resources/simple_dll/bld.inf -c armv6"
-	t.targets = [
+
+	rootcommand = "sbs -b smoke_suite/test_resources/simple_dll/bld.inf"
+	targets = [
 		"$(EPOCROOT)/epoc32/release/armv6/udeb/createstaticdll.dll.sym",
 		"$(EPOCROOT)/epoc32/release/armv6/urel/createstaticdll.dll.sym",
 		"$(EPOCROOT)/epoc32/release/armv5/lib/createstaticdll.dso",
@@ -29,15 +28,41 @@ def run():
 		"$(EPOCROOT)/epoc32/release/armv6/udeb/createstaticdll.dll",
 		"$(EPOCROOT)/epoc32/release/armv6/urel/createstaticdll.dll"
 		]
-	t.antitargets = [
+	antitargets = [
 		"$(EPOCROOT)/epoc32/release/armv5/lib/createstaticdll.lib",
 		"$(EPOCROOT)/epoc32/release/armv5/lib/createstaticdll{000a0000}.lib"
 		]
-	t.addbuildtargets('smoke_suite/test_resources/simple_dll/bld.inf', [
+	buildtargets = [
 		"createstaticdll_dll/armv6/udeb/CreateStaticDLL.o",
 		"createstaticdll_dll/armv6/urel/CreateStaticDLL.o",
 		"createstaticdll_dll/armv6/udeb/armv6_specific.o",
 		"createstaticdll_dll/armv6/urel/armv6_specific.o"
-	])
+	]
+	
+	t.id = "0097a"
+	t.name = "dll_armv6_rvct"
+	t.command = rootcommand + " -c armv6"
+	t.targets = targets
+	t.antitargets = antitargets
+	t.addbuildtargets("smoke_suite/test_resources/simple_dll/bld.inf", buildtargets)
 	t.run()
+
+	t.id = "0097b"
+	t.name = "dll_armv6_clean"
+	t.command = rootcommand + " -c armv6 clean"
+	t.targets = []
+	t.antitargets = []
+	t.run()
+
+	t.id = "0097c"
+	t.name = "dll_armv6_gcce"
+	t.command = rootcommand + " -c arm.v6.udeb.gcce4_3_2 -c arm.v6.urel.gcce4_3_2"
+	t.targets = targets
+	t.antitargets = antitargets
+	t.addbuildtargets("smoke_suite/test_resources/simple_dll/bld.inf", buildtargets)
+	t.run()
+
+	t.id = "97"
+	t.name = "dll_armv6"
+	t.print_result()
 	return t
---
+++ sbsv2/raptor/test/smoke_suite/test_resources/mmp/apply/test_mmp_apply.cpp
@@ -0,0 +1,9 @@
+
+#include "e32def.h"
+
+char test[] = "test mmp keyword APPLY";
+
+TInt E32Main()
+{
+	return 0;
+}
