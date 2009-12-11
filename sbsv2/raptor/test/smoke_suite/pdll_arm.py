49bb480cd51dbd37ee420890e83b2049ab797a31
Iain Williamson <iain.williamson@nokia.com>
2009-12-11 17:29:57 +0000

    Review rework: Removing result trackers
    --HG--
    branch : wip

--- sbsv2/raptor/test/smoke_suite/pdll_arm.py
+++ sbsv2/raptor/test/smoke_suite/pdll_arm.py
@@ -19,7 +19,6 @@ from raptor_tests import AntiTargetSmokeTest
 def run():
 	t = AntiTargetSmokeTest()
 	t.usebash = True
-	result = AntiTargetSmokeTest.PASS
 	
 	command = "sbs -b smoke_suite/test_resources/simple_dll/pbld.inf -c %s -f -"
 	maintargets = [
@@ -35,7 +34,7 @@ def run():
 	abiv1libtargets = [
 		"$(EPOCROOT)/epoc32/release/%s/lib/createstaticpdll.lib",
 		"$(EPOCROOT)/epoc32/release/%s/lib/createstaticpdll{000a0000}.lib"
-		]	
+		]
 	buildtargets =  [
 		"createstaticpdll_dll/%s/udeb/CreateStaticDLL.o",
 		"createstaticpdll_dll/%s/urel/CreateStaticDLL.o"
@@ -43,7 +42,7 @@ def run():
 	mustmatch = [
 		r".*\busrt\d_\d\.lib\b.*",
 		r".*\bscppnwdl\.dso\b.*"
-			]
+		]
 	mustnotmatch = [
 		".*ksrt.*"
 		]
@@ -62,8 +61,6 @@ def run():
 	if t.result == AntiTargetSmokeTest.SKIP:
 		t.targets.extend(map(lambda x: x % "armv5", abiv1libtargets))
 		t.run("windows")
-	if t.result == AntiTargetSmokeTest.FAIL:
-		result = AntiTargetSmokeTest.FAIL
 		
 	t.id = "000xb"
 	t.name = "pdll_armv5_clean"
@@ -71,10 +68,8 @@ def run():
 	t.targets = []
 	t.mustmatch = []
 	t.mustnotmatch = []
-	t.run()	
-	if t.result == AntiTargetSmokeTest.FAIL:
-		result = AntiTargetSmokeTest.FAIL		
-		
+	t.run()
+	
 	t.id = "000xc"
 	t.name = "pdll_armv5_gcce"
 	t.command = command % "gcce_armv5"
@@ -83,9 +78,7 @@ def run():
 	t.addbuildtargets('smoke_suite/test_resources/simple_dll/pbld.inf', map(lambda p: p % "armv5", buildtargets))
 	t.mustmatch = mustmatch
 	t.mustnotmatch = mustnotmatch
-	t.run()	
-	if t.result == AntiTargetSmokeTest.FAIL:
-		result = AntiTargetSmokeTest.FAIL
+	t.run()
 
 	t.id = "000xd"
 	t.name = "pdll_armv5_gcce_clean"
@@ -93,9 +86,7 @@ def run():
 	t.targets = []
 	t.mustmatch = []
 	t.mustnotmatch = []
-	t.run()	
-	if t.result == AntiTargetSmokeTest.FAIL:
-		result = AntiTargetSmokeTest.FAIL
+	t.run()
 
 	t.id = "000xe"
 	t.name = "pdll_armv7_rvct"
@@ -105,19 +96,15 @@ def run():
 	t.mustmatch = mustmatch
 	t.mustnotmatch = mustnotmatch
 	t.run()
-	if t.result == AntiTargetSmokeTest.FAIL:
-		result = AntiTargetSmokeTest.FAIL
-		
+	
 	t.id = "000xf"
 	t.name = "pdll_armv7_clean"
 	t.command = command % "armv7" + " clean"
 	t.targets = []
 	t.mustmatch = []
 	t.mustnotmatch = []
-	t.run()	
-	if t.result == AntiTargetSmokeTest.FAIL:
-		result = AntiTargetSmokeTest.FAIL		
-		
+	t.run()
+	
 	t.id = "000xg"
 	t.name = "pdll_armv7_gcce"
 	t.command = command % "arm.v7.udeb.gcce4_3_2 -c arm.v7.urel.gcce4_3_2"
@@ -126,12 +113,9 @@ def run():
 	t.addbuildtargets('smoke_suite/test_resources/simple_dll/pbld.inf', map(lambda p: p % "armv7", buildtargets))
 	t.mustmatch = mustmatch
 	t.mustnotmatch = mustnotmatch
-	t.run()	
-	if t.result == AntiTargetSmokeTest.FAIL:
-		result = AntiTargetSmokeTest.FAIL
+	t.run()
 
 	t.id = "x"
 	t.name = "pdll_arm"
-	t.result = result
 	t.print_result()
 	return t
