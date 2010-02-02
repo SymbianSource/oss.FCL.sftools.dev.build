84269939ddc584f5385ff6dd0d88db78900cd735
Iain Williamson <iain.williamson@nokia.com>
2010-02-02 16:08:55 +0000

    Adding extra source file to TC winscw test code, and fixing it so it works
    --HG--
    branch : fix

--- sbsv2/raptor/test/smoke_suite/test_resources/tracecompiler/TC_winscw/tcwinsfiletwo.cpp
+++ sbsv2/raptor/test/smoke_suite/test_resources/tracecompiler/TC_winscw/tcwinsfiletwo.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2000-2009 Nokia Corporation and/or its subsidiary(-ies).
+* Copyright (c) 2000-2010 Nokia Corporation and/or its subsidiary(-ies).
 * All rights reserved.
 * This component and the accompanying materials are made available
 * under the terms of the License "Eclipse Public License v1.0"
@@ -17,11 +17,11 @@
 */
 
 
-#include "tcwinsStaticDLL.h"
+#include "tcwinsfiletwo.h"
 #include <e32uid.h>
 #include "OstTraceDefinitions.h"
 #ifdef OST_TRACE_COMPILER_IN_USE
-#include "tcwinsStaticDLLTraces.h"
+#include "tcwinsfiletwoTraces.h"
 #endif
 
 
@@ -30,30 +30,22 @@
 
 extern "C" void __ARM_switch8();
 
-void sbs_test()
+EXPORT_C CMessenger2* CMessenger2::NewLC(CConsoleBase& aConsole, const TDesC& aString)
 	{
-	__ARM_switch8();
-	}
-
-
-EXPORT_C CMessenger* CMessenger::NewLC(CConsoleBase& aConsole, const TDesC& aString)
-	{
-	OstTrace0( TRACE_API, CMESSENGER_NEWL, "CMessenger::NewLC()" );
-	OstTrace0( TRACE_NORMAL, DUP1_CMESSENGER_NEWL, "CMessenger::NewLC()" );  
-	CMessenger* self=new (ELeave) CMessenger(aConsole);
+	OstTrace0( TRACE_NORMAL, DUP1_CMESSENGER2_NEWL, "CMessenger2::NewLC()" );  
+	CMessenger2* self=new (ELeave) CMessenger2(aConsole);
 	CleanupStack::PushL(self);
 	self->ConstructL(aString);
 	return self;
 	}
 
-CMessenger::~CMessenger() // destruct - virtual, so no export
+CMessenger2::~CMessenger2() // destruct - virtual, so no export
 	{
-	OstTrace0( TRACE_API, DUP1_CMESSENGER_CMESSENGER, "CMessenger::~CMessenger()" );
-	OstTrace0( TRACE_NORMAL, DUP2_CMESSENGER_CMESSENGER, "CMessenger::~CMessenger()" );  
+	OstTrace0( TRACE_API, DUP1_CMESSENGER2_CMESSENGER2, "CMessenger2::~CMessenger2()" );
 	delete iString;
 	}
 
-EXPORT_C void CMessenger::ShowMessage()
+EXPORT_C void CMessenger2::ShowMessage()
 	{
 	_LIT(KFormat1,"%S\n");
 	iConsole.Printf(KFormat1, iString); // notify completion
@@ -62,12 +54,12 @@ EXPORT_C void CMessenger::ShowMessage()
 // constructor support
 // don't export these, because used only by functions in this DLL, eg our NewLC()
 
-CMessenger::CMessenger(CConsoleBase& aConsole) // first-phase C++ constructor
+CMessenger2::CMessenger2(CConsoleBase& aConsole) // first-phase C++ constructor
 	: iConsole(aConsole)
 	{
 	}
 
-void CMessenger::ConstructL(const TDesC& aString) // second-phase constructor
+void CMessenger2::ConstructL(const TDesC& aString) // second-phase constructor
 	{
 	iString=aString.AllocL(); // copy given string into own descriptor
     }
