51f97eded207e14a7578dce4cc8118b064e1b0b7
Daniel Jacobs <daniel.jacobs@nokia.com>
2010-03-04 13:16:03 +0000

    Apply review comments to tidy up sbs_env.bat.
    --HG--
    branch : wip

--- sbsv2/raptor/bin/sbs_env.bat
+++ sbsv2/raptor/bin/sbs_env.bat
@@ -18,12 +18,12 @@
 @SET HOSTPLATFORM_DIR=win32
 
 @REM Automatically find SBS_HOME if it is not set
-@IF NOT "%SBS_HOME%"==""  goto foundhome
+@IF NOT "%SBS_HOME%"=="" GOTO foundhome
 @SET RAPTORBINDIR=%~dp0
-@SET WD=%cd%
-@cd /d %RAPTORBINDIR%\..
-@SET SBS_HOME=%cd%
-@cd /d %WD%
+@SET WD=%CD%
+@CD /d %RAPTORBINDIR%\..
+@SET SBS_HOME=%CD%
+@CD /d %WD%
 :foundhome 
 
 @REM The python and PYTHONPATH used by Raptor are determined by, in order of precedence:
