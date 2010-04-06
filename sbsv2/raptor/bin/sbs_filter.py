0988319a4fabf9d1776bcc215116855fe0ed9f16
Richard Taylor <richard.i.taylor@nokia.com>
2010-04-06 10:02:34 +0100

    apply review comments
    --HG--
    branch : wip

--- sbsv2/raptor/bin/sbs_filter.py
+++ sbsv2/raptor/bin/sbs_filter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+# Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
 # All rights reserved.
 # This component and the accompanying materials are made available
 # under the terms of the License "Symbian Foundation License v1.0"
