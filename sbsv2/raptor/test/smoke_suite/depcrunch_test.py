c0975b0b6d069442c1df7a3af03a1c4a49dd2eda
timothy.murphy@nokia.com <timothy.murphy@nokia.com>
2010-05-11 20:27:01 +0100

    fix: make sure depcrunch test can fail
    --HG--
    branch : fix

--- sbsv2/raptor/test/smoke_suite/depcrunch_test.py
+++ sbsv2/raptor/test/smoke_suite/depcrunch_test.py
@@ -25,17 +25,17 @@ def run():
 	t.id = "43562999"
 	t.name = "depcrunch"
 	t.command = "python $SBS_HOME/bin/depcrunch.py --extensions mbg,rsg --assume EPOCROOT < smoke_suite/test_resources/depcrunch/dep2.rpp.d"
-	t.stdout = [
-		r"EPOCROOT/epoc32/build/resource/c_98665870f0168225/dependentresource_/dependentresource_dependentresource_sc.rpp: \\",
-		r" EPOCROOT/testresource1.mbg \\",
-		r" EPOCROOT/testresource2.rsg \\",
-		r" EPOCROOT/testresource3.rsg \\",
-		r" EPOCROOT/testresource4.mbg \\",
-		r" EPOCROOT/testresource5.rsg \\",
-		r" EPOCROOT/testresource6.mbg \\",
-		r" EPOCROOT/testresource7.rsg \\",
-		r" EPOCROOT/testresource8.mbg \\",
-		r" EPOCROOT/testresource9.rsg "
+	t.mustmatch_multiline = [
+		r"EPOCROOT/epoc32/build/resource/c_98665870f0168225/dependentresource_/dependentresource_dependentresource_sc.rpp: \\\n"+
+		r" EPOCROOT/testresource1.mbg \\\n"+
+		r" EPOCROOT/testresource2.rsg \\\n"+
+		r" EPOCROOT/testresource3.rsg \\\n"+
+		r" EPOCROOT/testresource4.mbg \\\n"+
+		r" EPOCROOT/testresource5.rsg \\\n"+
+		r" EPOCROOT/testresource6.mbg \\\n"+
+		r" EPOCROOT/testresource7.rsg \\\n"+
+		r" EPOCROOT/testresource8.mbg \\\n"+
+		r" EPOCROOT/testresource9.rsg \n"
 		]
 	t.run()
 
