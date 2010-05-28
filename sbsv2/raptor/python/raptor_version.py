cef11769f3a5e983811e850c16170ae891aa593d
Iain Williamson <iain.williamson@nokia.com>
2010-05-28 15:15:09 +0100

    Version 2.14.1 [Candidate 1]

--- sbsv2/raptor/python/raptor_version.py
+++ sbsv2/raptor/python/raptor_version.py
@@ -16,7 +16,7 @@
 
 # replace CHANGESET with the Hg changeset for ANY release
 
-version=(2,14,0,"2010-05-19","symbian build system","CHANGESET")
+version=(2,14,1,"2010-06-01","symbian build system","CHANGESET")
 
 def numericversion():
 	"""Raptor version string"""
